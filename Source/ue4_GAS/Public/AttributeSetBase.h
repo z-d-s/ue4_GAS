85e33328720e97b8759517c2ed1c3e45def73afb
zhangdiansong <lmtxzds@126.com>
2022-09-21 07:37:51 +0800

    a

---
+++ Source/ue4_GAS/Public/AttributeSetBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AttributeSetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_GAS_API UAttributeSetBase : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
